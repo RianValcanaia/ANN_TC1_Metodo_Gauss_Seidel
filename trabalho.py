cf2ca4e4464d6437ffc9e3089d2ceaf389483b57
Lucas Macedo <117371383+lucasomac0@users.noreply.github.com>
2025-04-15 21:03:11 -0300

    Exclusão da função Jacobi, implementação do Vetor Resíduo e correções do teste de convergência

--- trabalho.py
+++ trabalho.py
@@ -11,32 +11,26 @@ criterio_parada = 0.0005
 x_inicial = [-0.3, 1.3, 2.8,-2.3]
 
 # calculo Gauss-seidel
-#### primeiro: ajustar os as linhas para convergência (não sei se essa é a melhor solução)
-def testeConvergencia(A):
-    for i in range(len(A)):
-        soma = 0
-        melhor_linha = i
-        for j in range(len(A)):
-            if i != j:
-                soma += abs(A[i][j])
-
-        if A[i][i] < soma:
-            melhor_criterio = abs(A[i][i]) - soma
-
-            for k in range (i+1, len(A)):
-                soma_k = 0
-                for j in range (len(A)):
-                    if i != j:
-                        soma_k += abs(A[k][j])
-
-                criterio_k = abs(A[k][i]) - soma_k
-
-                if criterio_k > melhor_criterio:
-                    melhor_criterio = criterio_k
-                    melhor_linha = k
-
-        if melhor_linha != i:
-            A[i], A[melhor_linha] = A[melhor_linha], A[i]
+#### primeiro: ajustar os as linhas para convergência 
+for i in range(len(A)):
+    soma = 0
+    melhor_linha = i
+    for j in range(len(A)):
+        if i != j:
+            soma += abs(A[i][j])
+    if abs(A[i][i]) < soma:
+        melhor_criterio = abs(A[i][i]) - soma
+        for k in range (i+1, len(A)):
+            soma_k = 0
+            for j in range (len(A)):
+                if k != j:
+                    soma_k += abs(A[k][j])
+            criterio_k = abs(A[k][i]) - soma_k
+            if criterio_k > melhor_criterio:
+                melhor_criterio = criterio_k
+                melhor_linha = k
+    if melhor_linha != i:
+        A[i], A[melhor_linha] = A[melhor_linha], A[i]
 
 #### descobrindo a precisao necessaria
 expoente = 0
@@ -89,33 +83,21 @@ while(diferenca_maior > criterio_parada):
 for valor in x_resultado:
     print(f"{valor:.5f}")
 
-# calculo pelo método de jacobi, com teste de convergência, trocas de linha
-def jacobi(A, b, x_inicial, criterio_parada, precisao):
-    x_resultado = x_inicial[:] # cria uma cópia do vetor resultado
-    diferenca_maior = float('inf') # garantindo que diferenca_maior > criterio_parada na primeira iteracao para que haja pelo menos uma iteracao
+# vetor residuo:
+# A.x̄ - b -> vai usar a matriz A e multiplica com o vetor x_resultado, depois faz a diferença com o vetor b
 
-    while diferenca_maior > criterio_parada:
-        x_anterior = x_resultado[:]  # cria uma cópia dos valores antigos
-        diferenca_maior = 0
+def qualidadeAjusteVetorResiduo(A, b, x_resultado):
+    vetResiduo = [] # apenas a declaração do vetor
+    for i in range (len(x_resultado)): # iterador das linhas
+        soma = 0 # soma resultante da multiplicação de matrizes
+        for j in range(len(A[i])): # iterador das colunas, poderia iterar utilizando o mesmo parâmetro do iterador das linhas (o tamanho do vetor resultado, que sempre terá como tamanho a quantidade de linhas da matriz A, se não não tem como fazer o produto matricial), mas ao utilizar o tamanho da linha é possível incluir matrizes nao quadradas na solução. Esse foi um problema que percebi nos testes.
+            soma += A[i][j] * x_resultado[j] # produto matricial
+        residuo = soma - b[i] # quando termino de iterar todos as colunas da matriz e eu ja tenho o resultado da primeira linha da matriz resultante, eu tiro a diferença com a linha respectiva do vetor de termos independentes.
+        vetResiduo.append(residuo) # no final apenas adiciona o residuo no fim do vetor residuo, o método .append() faz exatamente isso
+    for valor in vetResiduo: # printar o vetor residuo
+        print(f"{valor:.5f}") # .5f indica que é para formatar o print para 5 casas decimais
 
-        for i, linha in enumerate(A):
-            soma = 0 # inicializa a soma aqui pois ela zera a cada novo x calculado
-            for j, elemento in enumerate(linha):
-                if j != i: # se o elemento nao for da diagonal principal
-                    soma += elemento * x_anterior[j]  # usa os valores antigos na hora de somar
-
-            novo_valor = (b[i] - soma) / A[i][i] # formula
-            novo_valor = arredondamento(novo_valor, precisao) # arredonda
-            diferenca = abs(x_anterior[i] - novo_valor) # calcula a diferenca
-
-            if diferenca > diferenca_maior:
-                diferenca_maior = diferenca
-
-            x_resultado[i] = novo_valor  # atualiza só depois de calcular tudo
-
-    return x_resultado
-
-# vetor residuo;
+qualidadeAjusteVetorResiduo(A, b, x_resultado) # chama a função do vetor residuo
 
 
 
